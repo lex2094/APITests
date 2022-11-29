5dd83f96f5be76b97da5cb4bda0bc53d1e191fb4
lex2094 <alesha1240@mail.ru>
2022-11-29 11:05:59 +0300

    add restAssured

--- src/test/java/api/RegresTest.java
+++ src/test/java/api/RegresTest.java
@@ -1,11 +1,15 @@
 package api;
 
+import dev.failsafe.internal.util.Assert;
 import io.restassured.http.ContentType;
+import io.restassured.internal.common.assertion.Assertion;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 
@@ -14,12 +18,21 @@ public class RegresTest{
 
     @Test
     public void checkAvatarAndIdTest(){
+//        Specification.installSpecification(Specification.requestSpec(URL),);
         List<UserData> users = given().
                 when()
-                .contentType(ContentType.JSON)
-                .get(URL + "/api/users?page=2")
+                .get("/api/users?page=2")
                 .then().log().all()
                 .extract().body().jsonPath().getList("data", UserData.class);
-        System.out.println("1");
+
+        users.stream().forEach(x -> Assertions.assertTrue(x.getAvatar().contains(x.getId().toString())));
+        Assertions.assertTrue(users.stream().allMatch(x ->x.getEmail().endsWith("@regres.in")));
+
+        List<String> avatars = users.stream().map(UserData::getAvatar).collect(Collectors.toList());
+        List<String> ids = users.stream().map(x -> x.getId().toString()).collect(Collectors.toList());
+        for (int i = 0; i < avatars.size(); i++) {
+            Assertions.assertTrue(avatars.get(i).contains(ids.get(i)));
+
+        }
     }
 }
